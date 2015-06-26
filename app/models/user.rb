f25c8005564bf7d8183250fe3751d1bb40852dbe
Bhanu Prasad <bhanuprasad143@gmail.com>
2015-06-26 21:33:16 +0700

    remove acts_as_authorization_*; add associations; add methods for checking role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
-  acts_as_authorization_subject
   enum subscription_status: [:abandoned, :active, :cancelled]
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :encryptable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  belongs_to :role
 
   validates :email, :first_name, :last_name, :company, presence: true
   validates :email, uniqueness: true
@@ -19,6 +20,26 @@ class User < ActiveRecord::Base
   def name(response_required = false)
     name = "#{first_name} #{last_name}".strip
     name.blank? && response_required ? 'Totem User' : name
+  end
+
+  def admin?
+    self.role.try(:name) == "admin"
+  end
+
+  def subscriber?
+    self.role.try(:name) == "subscriber"
+  end
+
+  def super_admin?
+    self.role.try(:name) == "super_admin"
+  end
+
+  def manage?
+    %w(admin super_admin).include?(self.role.try(:name))
+  end
+
+  def has_role!(role_name)
+    self.update_attributes(role: Role.find_or_create_by(name: role_name))
   end
 
   alias :subscription_status_cancelled! :cancelled!
